a156bc69d038f7823eab20aead5f7061938a1efa
Elias Teikari <elias.tkri@gmail.com>
2025-11-26 23:03:27 +0200

    removed react

--- frontend/app/routes.ts
+++
@@ -1,5 +0,0 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
-import Home from "./routes/home";
-import Root from "./root";
-
-export default [index("routes/home.tsx")] satisfies RouteConfig;
