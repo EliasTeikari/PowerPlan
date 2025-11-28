1fc9db0956a243ce749f55d9db19128ccf0d6ab4
Elias Mikael Teikari <EliasTeikari@Margits-iMac-Pro-2.local>
2025-11-28 23:56:30 +0200

    added react bits background

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # PowerPlan
+
 A gym app that started out as a school project, but I wanted to make a full-stack app that I would actually use.
 
 # Tech-Stack
+
 Frontend: React
-UI: Tailwind
+UI: Bootstrap
 Backend: Node.js + Express.js
 Database: Mongodb
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,64 @@
 import { Link } from "react-router-dom";
+import Dither from "./components/Dither";
 
 function App() {
     return (
         <>
-            <div className="full-page bg-black text-white">
-                <h1 className="text-white p-3 d-flex justify-content-center">
-                    {" "}
-                    WORKOUTS
-                </h1>
-                <div className="d-flex flex-column m-4">
-                    <Link
-                        to="/push"
-                        type="button"
-                        className="btn btn-light btn-lg p-5 mb-3"
-                    >
-                        PUSH
-                    </Link>
-                    <Link
-                        to="/pull"
-                        type="button"
-                        className="btn btn-light btn-lg p-5 mb-3"
-                    >
-                        PULL
-                    </Link>
-                    <Link
-                        to="/legs"
-                        type="button"
-                        className="btn btn-light btn-lg p-5 mb-3"
-                    >
-                        LEGS
-                    </Link>
-                    <Link
-                        to="/arms"
-                        type="button"
-                        className="btn btn-light btn-lg p-5 mb-3"
-                    >
-                        ARMS
-                    </Link>
+            <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+                <Dither
+                    waveColor={[0.5, 0.5, 0.5]}
+                    disableAnimation={false}
+                    enableMouseInteraction={false}
+                    mouseRadius={0.3}
+                    colorNum={4}
+                    waveAmplitude={0.3}
+                    waveFrequency={3}
+                    waveSpeed={0.05}
+                />
+                <div style={{ 
+                    position: 'absolute', 
+                    top: 0, 
+                    left: 0, 
+                    width: '100%', 
+                    height: '100%', 
+                    zIndex: 1,
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}>
+                    <h1 className="text-white p-3 d-flex justify-content-center">
+                        {" "}
+                        WORKOUTS
+                    </h1>
+                    <div className="d-flex flex-column m-4">
+                        <Link
+                            to="/push"
+                            type="button"
+                            className="btn btn-light btn-lg p-5 mb-3"
+                        >
+                            PUSH
+                        </Link>
+                        <Link
+                            to="/pull"
+                            type="button"
+                            className="btn btn-light btn-lg p-5 mb-3"
+                        >
+                            PULL
+                        </Link>
+                        <Link
+                            to="/legs"
+                            type="button"
+                            className="btn btn-light btn-lg p-5 mb-3"
+                        >
+                            LEGS
+                        </Link>
+                        <Link
+                            to="/arms"
+                            type="button"
+                            className="btn btn-light btn-lg p-5 mb-3"
+                        >
+                            ARMS
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
