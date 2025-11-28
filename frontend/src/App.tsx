1b3c482bf058a235f90b1472376a5448ba771e01
Elias Mikael Teikari <EliasTeikari@Margits-iMac-Pro-2.local>
2025-11-28 23:44:36 +0200

    Landing page looking nice

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,39 +3,41 @@ import { Link } from "react-router-dom";
 function App() {
     return (
         <>
-            <h1 className="text-bg-secondary p-3 d-flex justify-content-center">
-                {" "}
-                WORKOUTS
-            </h1>
-            <div className="d-flex">
-                <Link
-                    to="/push"
-                    type="button"
-                    className="btn btn-light btn-lg p-2 flex-fill"
-                >
-                    PUSH
-                </Link>
-                <Link
-                    to="/pull"
-                    type="button"
-                    className="btn btn-light btn-lg p-2 flex-fill"
-                >
-                    PULL
-                </Link>
-                <Link
-                    to="/legs"
-                    type="button"
-                    className="btn btn-light btn-lg p-2 flex-fill"
-                >
-                    LEGS
-                </Link>
-                <Link
-                    to="/arms"
-                    type="button"
-                    className="btn btn-light btn-lg p-2 flex-fill"
-                >
-                    ARMS
-                </Link>
+            <div className="full-page bg-black text-white">
+                <h1 className="text-white p-3 d-flex justify-content-center">
+                    {" "}
+                    WORKOUTS
+                </h1>
+                <div className="d-flex flex-column m-4">
+                    <Link
+                        to="/push"
+                        type="button"
+                        className="btn btn-light btn-lg p-5 mb-3"
+                    >
+                        PUSH
+                    </Link>
+                    <Link
+                        to="/pull"
+                        type="button"
+                        className="btn btn-light btn-lg p-5 mb-3"
+                    >
+                        PULL
+                    </Link>
+                    <Link
+                        to="/legs"
+                        type="button"
+                        className="btn btn-light btn-lg p-5 mb-3"
+                    >
+                        LEGS
+                    </Link>
+                    <Link
+                        to="/arms"
+                        type="button"
+                        className="btn btn-light btn-lg p-5 mb-3"
+                    >
+                        ARMS
+                    </Link>
+                </div>
             </div>
         </>
     );
